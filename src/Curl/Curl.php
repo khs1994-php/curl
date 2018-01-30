783caf3209d34382587db77badf42fb4b2301b50
khs1994 <khs1994@khs1994.com>
2018-01-30 14:55:23 +0800

    Update

--- src/Curl/Curl.php
+++ src/Curl/Curl.php
@@ -4,15 +4,27 @@ namespace Curl;
 
 class Curl
 {
+    const VERSION = 'v18.03';
+
+    const TIMEOUT = 20;
+
     public $url;
 
     public $ch;
+
+    private $headers=[];
 
     public function __construct(string $url= null)
     {
         $this->ch = curl_init();
         $this->setUrl($url);
         $this->setOpt(CURLOPT_RETURNTRANSFER, 1);
+        $this->setTimeout(self::TIMEOUT);
+    }
+
+    public function setTimeout(int $sec)
+    {
+        $this->setOpt(CURLOPT_TIMEOUT, $sec);
     }
 
     public function setOpt(string $opt, $value)
@@ -20,21 +32,36 @@ class Curl
         curl_setopt($this->ch, $opt, $value);
     }
 
+    public function setOptArray(array $array=[])
+    {
+        curl_setopt_array($this->ch, $array);
+    }
+
     public function setUrl(string $url=null)
     {
-        $this->url=$url;
+        if (!$this->url) {
+            $this->url=$url;
+        }
         $this->setOpt(CURLOPT_URL, $this->url);
     }
 
-    public function setHeader(array $header=[])
+    public function setHeader(string $name, string $value)
     {
-        $this->setOpt(CURLOPT_HTTPHEADER, $header);
+        $this->headers[$name]=$value;
+        $headers=[];
+        foreach ($this->headers as $key => $value) {
+            $headers[]=$name.':'.$value;
+        }
+
+        $this->setOpt(CURLOPT_HTTPHEADER, $headers);
     }
 
     public function setCA(string $ca)
     {
-        $this->setOpt(CURLOPT_CAINFO, './config/ssl/ca.pem');
+        $this->setOpt(CURLOPT_CAINFO, $ca);
     }
+
+    // Support Docker Daemon TLS
 
     public function docker(string $ca, string $key, string $cert)
     {
@@ -46,25 +73,21 @@ class Curl
 
     public function get(string $url=null, string $data = null)
     {
-        $header = ['content-type: application/x-www-form-urlencoded;charset=UTF-8'];
-
         if ($data) {
             $this->setUrl($url.'?'.$data);
         } else {
             $this->setUrl($url);
         }
 
-        $this->setHeader($header);
+        $this->setHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
 
         return $this->exec();
     }
 
     public function post(string $url=null, string $data = null)
     {
-        $header = ['content-type: application/x-www-form-urlencoded;charset=UTF-8'];
-
         $this->setUrl($url);
-        $this->setHeader($header);
+        $this->setHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
         $this->setOpt(CURLOPT_POST, 1);
         $this->setOpt(CURLOPT_POSTFIELDS, $data);
 
@@ -89,7 +112,21 @@ class Curl
     {
         $output = curl_exec($this->ch);
 
+        $errorCode=curl_errno($this->ch);
+        $errorMssage=curl_error($this->ch);
+
+        curl_close($this->ch);
+
+        if ($errorCode) {
+            return [$errorCode=>$errorMssage];
+        }
+
         return $output;
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
     }
 
     // 魔术方法，把对象当做函数调用
@@ -104,12 +141,5 @@ class Curl
     public function __toString()
     {
         return $this->get($this->url);
-    }
-
-    // 析构函数
-
-    public function __destruct()
-    {
-        curl_close($this->ch);
     }
 }
